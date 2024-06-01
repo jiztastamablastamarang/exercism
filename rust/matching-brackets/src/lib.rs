859e0bb0cab8fccef86956d7adbac5076da2d7c5
tyrzen <jiztastamablastamarang@gmail.com>
2024-06-01 07:29:45 +0300

    bulk upd

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,22 +1,15 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    return eliminate_brackets(filter_brackets(string)).is_empty();
-}
+    let mut stack = Vec::new();
 
-fn filter_brackets(string: &str) -> String {
-    return string
-        .chars()
-        .filter(|&c| matches!(c, '(' | ')' | '{' | '}' | '[' | ']'))
-        .collect();
-}
-
-fn eliminate_brackets(mut string: String) -> String {
-    for pair in ["()", "{}", "[]"] {
-        if let Some(pos) = string.find(pair) {
-            string.replace_range(pos..pos + 2, "");
-            return eliminate_brackets(string);
+    for bracket in string.chars() {
+        match bracket {
+            '(' | '[' | '{' => stack.push(bracket),
+            ')' if stack.pop() != Some('(') => return false,
+            ']' if stack.pop() != Some('[') => return false,
+            '}' if stack.pop() != Some('{') => return false,
+            _ => (),
         }
     }
 
-    return string;
+    return stack.is_empty();
 }
-
