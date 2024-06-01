6b043cba9f1d8f0a1fedf23f15be2801592d1699
tyrzen <jiztastamablastamarang@gmail.com>
2024-06-01 07:27:45 +0300

    collatz

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn collatz(mut n: u64) -> Option<u64> {
+    return match n {
+        0 => None,
+        1 => Some(0),
+        _ if n % 2 == 0 => Some(1 + collatz(n / 2)?),
+        _ => Some(1 + collatz(3 * n + 1)?),
+    };
+}
